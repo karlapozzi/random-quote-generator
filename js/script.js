f874d5c74228c8e91e6db4a53786b58b95e2a12b
karla <karla@getharvest.com>
2021-01-28 16:21:05 -0800

    Add quotes

--- js/script.js
+++ js/script.js
@@ -11,6 +11,38 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+//"Famous" quotes from Peloton instructors found on pinterest and fan blogs
+
+let quotes = [
+  {
+    quote: `Drop your shoulders, and drop your baggage.`,
+    source: `Christine D'Ercole`
+  },
+  {
+    quote: `Are you ready? Yes or yes?`,
+    source: `Ally Love`
+  },
+  {
+    quote: `If Britney can get through 2007, you can get up this hill!`,
+    source: `Cody Rigsby`
+  },
+  {
+    quote: `You are you and that is your superpower.`,
+    source: `Jess King`
+  },
+  {
+    quote: `We eat fear for breakfast.`,
+    source: `Robin Arzon`
+  }, 
+  {
+    quote: `If you can't be good, be careful.`,
+    source: `Denis Morton`
+  },
+  {
+    quote: `It's a good day to have a good day.`,
+    source: `Tunde Oyeneyin`
+  }
+];
 
 
 /***
